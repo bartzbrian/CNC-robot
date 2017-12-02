541b7e8625bc2ea59b1c1c53234f942b8e50a364
bartzb <brianbartz1@gmail.com>
2017-12-01 16:32:02 -0800

    WORKING
    Now working with position control on the X axis. Will move to wire and implement Y axis now.

--- CNC-code.ino
+++ CNC-code.ino
@@ -3,7 +3,17 @@ const int Xpulse = 6;
 const int Xdirection = 7;
 const int XlimitMin = 2;
 const int XlimitMax = 3;
-int XpulseLength = 0;
+long XpulseLength = 0;
+int dirX = 0;
+int prevLimitMax = 0;
+long currentPositionX = -1;
+
+long positions[3][2];
+int positionCounter = 0; //this will be replaced by sequencing logic
+int calculatingTargetPosition = 0;
+int calculatingPositions = 1;
+int movingToTarget = 0;
+long targetPos = 0;
 
 //set pinModes, run calibration sequence
 void setup() {
@@ -11,107 +21,137 @@ void setup() {
   pinMode(Xdirection,OUTPUT);
   pinMode(XlimitMin, INPUT);
   pinMode(XlimitMax, INPUT);
-  //Serial.begin(9600);
-  XpulseLength = calibrationX();
+  Serial.begin(9600);
+  measureX();
+  currentPositionX = XpulseLength;
 }
 
+
+void setDirectionX(int dir){
+  if( dir != dirX){
+    dirX = dir;
+    digitalWrite(Xdirection,dirX);
+  } 
+}
 
 
 // moves towards min until limit switch tripped, then moves towards max,
 // counting pulses until second limit switch tripped in order to get the
 // length of the whole X-axis actuator in pulses. Returns that length.
-int calibrationX(){
+void measureX(){
 
-    int pulseCount = 0;
-    int reachedEnd = 0;
+    int tripCheck = 0;
     
-    digitalWrite(Xdirection,LOW); //set direction towards min
+    setDirectionX(0); //set direction towards min
     
-    while(reachedEnd == 0){
-      
-      pulseX();
-      
-      if (digitalRead(XlimitMin) == 1){ //if limit switch tripped
-          reachedEnd = 1;
-          digitalWrite(Xdirection,HIGH); //switch direction
-          //Serial.println("MIN REACHED, CHANGING DIRECTION");
-        }
+    while(tripCheck != -1){    
+      tripCheck = pulseX();
     }
-    
-    reachedEnd = 0; //reset stop condition
-    //Serial.println("MOVING OTHER WAY");
 
-    int current = 0; 
-    int previous = 0; 
+    Serial.println("REACHED MIN, TURNING AROUND");
+    setDirectionX(1); // turn around
+    tripCheck = 0;// reset the tripCheck
+    Serial.println("MOVING OTHER WAY");
     
-    while(reachedEnd == 0){ // set direction towards max, this time counting pulses
-          
-      pulseX();
-      pulseCount++;
-      current = digitalRead(XlimitMax);
-      
-      if (current == 1 && previous == 1){   //if limit switch tripped
-          reachedEnd = 1;
-          //Serial.println("MAX REACHED");
-        } else {
-          previous = current; //keeps track of last two switch vals to account for noise on long wire
-        }        
+    while(tripCheck != -1){   
+      tripCheck = pulseX();
+      XpulseLength += tripCheck;
     }
      
-    //Serial.print("FINAL X-AXIS PULSE COUNT: ");
-    //Serial.println(pulseCount);
-    return pulseCount;
+    Serial.print("MAX REACHED, FINAL X-AXIS PULSE COUNT: ");
+    Serial.println(XpulseLength);
+    
   }
 
+//sends a pulse on the X-axis, returns one after a pulse cycle
+int pulseX() {
 
-
-//sends a pulse on the X-axis without blocking
-void pulseX() {
+    int maxX = 0;
     
-    unsigned long previous_time = micros();
-    unsigned long current_time = micros();
+    // safetyMin
+    if( dirX == 0 && digitalRead(XlimitMin) ){ 
+      return -1;
+    } else if( dirX == 1 ){
+      maxX = digitalRead(XlimitMax);
+      if( maxX ==1 && prevLimitMax == 1 ){
+        return -1;
+      }
+    }
+    prevLimitMax = maxX;
 
     digitalWrite(Xpulse,HIGH);
-    while(current_time - previous_time <= 5){
-        current_time = micros();
-      }
+    delayMicroseconds(5);
     digitalWrite(Xpulse,LOW);
-    previous_time = micros();
-    while(current_time - previous_time <= 5){
-        current_time = micros();
-      }
+    delayMicroseconds(5);
+    return 1;
+
   }
+  
+//int gotoPosition(int pos){
+//  
+//}
 
-
-
-// moves to 1/3,1/2,then 2/3 the X axis' pulse length (determined from calibration) 
-// in each direction on repeat, with 1 second delay between each movement. 
 void loop() { 
-    digitalWrite(Xdirection,LOW); 
-      for (int j = 0; j < (XpulseLength/3); j++){   
-          pulseX();
-        }
-      delay(1000);
-      for (int j = 0; j < (XpulseLength/2); j++){  
-          pulseX();
-        }
-      delay(1000);
-      for (int j = 0; j < (2*(XpulseLength/3)); j++){
-          pulseX();
-        }
-      delay(1000);
+  
+    if (calculatingPositions){
+      
+        positions[0][0] = (XpulseLength * .3333);
+        positions[1][0] = (XpulseLength * .5);
+        positions[2][0] = (XpulseLength * .6666);
+  
+        calculatingPositions = 0;
+        Serial.println(String(positions[0][0]));
+        calculatingTargetPosition = 1;
         
-    digitalWrite(Xdirection,HIGH); //turn around
-      for (int j = 0; j < (XpulseLength/3); j++){
-          pulseX();
+      }
+
+    if (calculatingTargetPosition){
+      
+      if (positionCounter > 2){//resets the sequence for now
+        positionCounter = 0;
         }
-      delay(1000);
-      for (int j = 0; j < (XpulseLength/2); j++){
-          pulseX();
+        
+      targetPos = positions[positionCounter][0]; //set the new target position
+      calculatingTargetPosition = 0; //causes this to not get 
+      positionCounter++;
+      movingToTarget = 1;
+      
+      if (targetPos > currentPositionX){
+        setDirectionX(1);
+      } else if (targetPos < currentPositionX){
+        setDirectionX(0);
+      }
+
+      Serial.print("new target position is: ");
+      Serial.print(targetPos);
+      Serial.print(", current position is: ");
+      Serial.print(currentPositionX);
+      Serial.print(", direction is set to: ");
+      Serial.println(dirX);
+        
+    }
+    
+   if (movingToTarget){
+    
+      if (targetPos == currentPositionX){
+          calculatingTargetPosition = 1;
+          movingToTarget = 0;
+          Serial.println("done moving to this position");
+          delay(1000);
         }
-      delay(1000);
-      for (int j = 0; j < (2*(XpulseLength/3)); j++){
-          pulseX();
-        }
-      delay(1000);
-  }
+
+      int tripCheck = 0;
+      tripCheck = pulseX();
+      if (tripCheck == -1){
+        Serial.println("switch tripped, aborting");
+        movingToTarget = 0;
+       } else {
+        if (dirX == 1){
+            currentPositionX++;
+          }else if (dirX == 0){
+            currentPositionX--;
+            }
+        }    
+    }
+    
+  }//end loop
